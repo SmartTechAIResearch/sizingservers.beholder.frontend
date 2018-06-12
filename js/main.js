650a6f7c67de35ab0fce52d45aa3f5eb654eb24a
Dieter Vandroemme <dietere.vandroemme@gmail.com>
2018-06-12 15:31:23 +0200

    Working implementation
    Needs some GUI stuff (VM nesting in hosts)  and encryption of
    credentials

--- js/main.js
+++ js/main.js
@@ -3,14 +3,123 @@
  * University College of West-Flanders, Department GKG
  * 
  */
-var endpoint = "http://localhost:5000/systeminformations"; //New dotnet framework api (includes VMware vHost hw monitoring)
+//var endpoint = "http://localhost:5000/systeminformations"; //New dotnet framework api (includes VMware vHost hw monitoring)
+//var endpointVMH = "http://localhost:5000/vmwarehosts"
 //var endpoint = "http://localhost:5000/api"; // dotnetcore api
+var endpoint = "http://localhost:28751/systeminformations"; //New dotnet framework api (includes VMware vHost hw monitoring)
+var endpointVH = "http://localhost:28751/vmwarehosts"
 var apiKey = "<insert a SHA-512 of a piece of text here>";
 var systemInformations = [];
+var vhSystemInformations = [];
 var newSystemInformationsCount = 0;
 var collapsed = true;
 //Templating without a template engine.
-var template = "<div class=\"si\" id=\"si{{}}\"><div class=\"siHeader\"><button class=\"btn\" id=\"siToggleCollapse{{}}\">></button>&nbsp;<label id=\"siHostname{{}}\"></label>&nbsp;<span id=\"siIPs{{}}\"></span><span id=\"siTimestamp{{}}\"></span></div><div class=\"siBody\" id=\"siBody{{}}\"><ul><li id=\"siOS{{}}\"></li><li id=\"siSystem{{}}\"> </li><li id=\"siBaseboard{{}}\"> </li><li id=\"siBios{{}}\"> </li><li id=\"siProcessors{{}}\"> </li><li id=\"siMemoryModules{{}}\"></li><li id=\"siDisks{{}}\"> </li><li id=\"siNics{{}}\"> </li></ul><button class=\"btn btn-default\" id=\"remove{{}}\"><img src=\"img/remove.png\" /></button></div></div>";
+var templateVHost = "<div class=\"si\" id=\"siVH{{}}\"><div class=\"siVHHeader\"><button class=\"btn\" id=\"siVHToggleCollapse{{}}\">></button>&nbsp;<label id=\"siVHIpOrHostname{{}}\"></label>&nbsp;<span id=\"siVHTimestamp{{}}\"></span></div><div class=\"siBody\" id=\"siVHBody{{}}\"><ul><li id=\"siVHOS{{}}\"></li><li id=\"siVHSystem{{}}\"> </li><li id=\"siVHBios{{}}\"> </li><li id=\"siVHProcessors{{}}\"> </li><li id=\"siVHMemoryInGB{{}}\"></li><li id=\"siVHDatastores{{}}\"></li><li id=\"siVHDiskPaths{{}}\"> </li><li id=\"siVHNics{{}}\"> </li></ul><button class=\"btn btn-default\" id=\"vhEdit{{}}\">...</button>&nbsp;<button class=\"btn btn-default\" id=\"vhRemove{{}}\"><img src=\"img/remove.png\" /></button></div><div class=\"vhGuestContainer\" id=\"vhGuestContainer{{}}\"></div></div>";
+var templateVM = "<div class=\"si\" id=\"si{{}}\"><div class=\"siHeader\"><button class=\"btn\" id=\"siToggleCollapse{{}}\">></button>&nbsp;<label id=\"siHostname{{}}\"></label>&nbsp;<span id=\"siIPs{{}}\"></span><span id=\"siTimestamp{{}}\"></span></div><div class=\"siBody\" id=\"siBody{{}}\"><ul><li id=\"siOS{{}}\"></li><li id=\"siSystem{{}}\"> </li><li id=\"siBaseboard{{}}\"> </li><li id=\"siBios{{}}\"> </li><li id=\"siProcessors{{}}\"> </li><li id=\"siMemoryModules{{}}\"></li><li id=\"siDisks{{}}\"> </li><li id=\"siNics{{}}\"> </li></ul><button class=\"btn btn-default\" id=\"remove{{}}\"><img src=\"img/remove.png\" /></button></div></div>";
+
+var vhSystemInformation = function (siVHJson) {
+    var _me = this;
+    var _ipOrHostname, _guests;
+
+    //Templating without a template engine.
+    ++newSystemInformationsCount;
+
+    var _siVH = "#siVH" + newSystemInformationsCount;
+    var _siVHToggleCollapse = "#siVHToggleCollapse" + newSystemInformationsCount;
+    var _siVHIpOrHostname = "#siVHIpOrHostname" + newSystemInformationsCount;
+    var _siVHTimestamp = "#siVHTimestamp" + newSystemInformationsCount;
+    var _siVHBody = "#siVHBody" + newSystemInformationsCount;
+    var _siVHOS = "#siVHOS" + newSystemInformationsCount;
+    var _siVHSystem = "#siVHSystem" + newSystemInformationsCount;
+    var _siVHBios = "#siVHBios" + newSystemInformationsCount;
+    var _siVHProcessors = "#siVHProcessors" + newSystemInformationsCount;
+    var _siVHMemoryInGB = "#siVHMemoryInGB" + newSystemInformationsCount;
+    var _siVHDatastores = "#siVHDatastores" + newSystemInformationsCount;
+    var _siVHDiskPaths = "#siVHDiskPaths" + newSystemInformationsCount;
+    var _siVHNics = "#siVHNics" + newSystemInformationsCount;
+
+    var _btnVHEdit = '#vhEdit' + newSystemInformationsCount;
+    var _btnVHRemove = '#vhRemove' + newSystemInformationsCount;
+
+    var vhGuestContainer = '#vhGuestContainer' + newSystemInformationsCount;
+
+
+    $('#container').append(templateVHost.replace(/{{}}/g, newSystemInformationsCount));
+
+    this.updateInfo = function (siVHJson) {
+        _ipOrHostname = siVHJson['ipOrHostname'];
+        _guests = siVHJson['guestHostnames'].replace(/\t/g, ', ');
+
+        $(_siVHIpOrHostname).text('VHOST ' + _ipOrHostname);
+
+        var ts = siVHJson['timeStampInSecondsSinceEpochUtc'];
+        if (ts < 10000000000) {
+            ts *= 1000 //seconds to ms.
+        }
+
+        ts = new Date(ts + (new Date().getTimezoneOffset() * -1)); //Utc to timezone.
+        $(_siVHTimestamp).text('(last updated: ' + ts + ')');
+
+
+        $(_siVHOS).text('OS: ' + siVHJson['os']);
+        $(_siVHSystem).text('System: ' + siVHJson['system']);
+        $(_siVHBios).text('BIOS: ' + siVHJson['bios']);
+        $(_siVHProcessors).text('Processors: ' + siVHJson['processors'].replace(/\t/g, ', ') + ' (Total ' + siVHJson['numCpuCores'] + ' cores, ' + siVHJson['numCpuThreads'] + ' threads)');
+        $(_siVHMemoryInGB).text('Memory: ' + siVHJson['memoryInGB'] + ' GB');
+        $(_siVHDatastores).text('Datastores: ' + siVHJson['datastores'].replace(/\t/g, ', '));
+        $(_siVHDiskPaths).text('Paths: ' + siVHJson['vDiskPaths'].replace(/\t/g, ', '));
+        $(_siVHNics).text('NICs: ' + siVHJson['nics'].replace(/\t/g, ', '));
+    };
+
+    this.ipOrHostname = function () {
+        return _ipOrHostname;
+    };
+
+    this.collapsed = function () {
+        return !$(_siVHBody).is(':visible');
+    };
+    this.collapse = function () {
+        $(_siVHBody).hide();
+        $(_siVHToggleCollapse).text('>');
+    };
+    this.uncollapse = function () {
+        $(_siVHBody).show();
+        $(_siVHToggleCollapse).text('V');
+    }
+
+    $(_siVHToggleCollapse).click(function () {
+        if ($(_siVHBody).is(':visible')) {
+            _me.collapse();
+        }
+        else {
+            _me.uncollapse();
+        }
+    });
+
+    $(_btnVHRemove).click(function () {
+        vhSystemInformations = jQuery.grep(vhSystemInformations, function (value) {
+            return value != _me;
+        });
+        $(_siVH).remove();
+
+        $.ajax({
+            url: endpointVH + "/remove?apiKey=" + apiKey + "&ipOrHostname=" + _ipOrHostname,
+            type: 'DELETE'
+        });
+
+    });
+
+    $(_btnVHEdit).click(function () {
+        $('#addEditVHost').show();
+        $('#vhIpOrHostname').val(_ipOrHostname);
+        $('#vhUsername').val('');
+        $('#vhPassword').val('');
+        $('#vhGuests').val(_guests);
+    });
+
+    this.collapse();
+    this.updateInfo(siVHJson);
+};
 
 var systemInformation = function (siJson) {
     var _me = this;
@@ -37,7 +146,7 @@ var systemInformation = function (siJson) {
     var _btnRemove = '#remove' + newSystemInformationsCount;
 
 
-    $('#container').append(template.replace(/{{}}/g, newSystemInformationsCount));
+    $('#container').append(templateVM.replace(/{{}}/g, newSystemInformationsCount));
 
     this.updateInfo = function (siJson) {
         _hostname = siJson['hostname'];
@@ -107,12 +216,18 @@ var systemInformation = function (siJson) {
 
 var refresh = function () {
     //Add or update the info on the GUI.
-    $.getJSON(endpoint + "/list?apiKey=" + apiKey, function (data) {
-        $.each(data, function (i, siJson) {
-            addOrUpdateSystemInformation(siJson);
+    $.getJSON(endpointVH + "/listsysteminformation?apiKey=" + apiKey, function (data) {
+        $.each(data, function (i, siVHJson) {
+            addOrUpdateVHSystemInformation(siVHJson);
         });
     }).always(function () {
-        sortSystemInformationByHostname();
+        $.getJSON(endpoint + "/list?apiKey=" + apiKey, function (data) {
+            $.each(data, function (i, siJson) {
+                addOrUpdateSystemInformation(siJson);
+            });
+        }).always(function () {
+            sortSystemInformationByHostname();
+        })
     });
 };
 
@@ -131,6 +246,22 @@ var addOrUpdateSystemInformation = function (siJson) {
     }
 };
 
+var addOrUpdateVHSystemInformation = function (siVHJson) {
+    var si = null;
+    $.each(vhSystemInformations, function (i, siCandidate) {
+        if (siCandidate.ipOrHostname() == siVHJson['ipOrHostname']) {
+            si = siCandidate;
+            si.updateInfo(siVHJson);
+            return false;
+        }
+    });
+
+    if (si == null) {
+        vhSystemInformations.push(new vhSystemInformation(siVHJson));
+    }
+};
+
+//Does not work yet
 var sortSystemInformationByHostname = function () {
     var $sis = $('#container').children();
     $sis.sort(function (siX, siY) {
@@ -150,6 +281,25 @@ var sortSystemInformationByHostname = function () {
     });
 
     $sis.detach().appendTo($('#container'));
+};
+
+var addEditVHost = function () {
+    var vmwareHostConnectionInfo = {
+        'ipOrHostname': $.trim($('#vhIpOrHostname').val()),
+        'guestHostnames': $.trim($('#vhGuests').val()).replace(/' '/g, '').replace(/'\t'/g, '').replace(/','/g, '\t'),
+        'username': $.trim($('#vhUsername').val()),
+        'password': $('#vhPassword').val()
+    };
+
+    $.ajax({
+        url: endpointVH + "/addorupdate?apiKey=" + apiKey,
+        data: JSON.stringify(vmwareHostConnectionInfo),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        type: 'POST'
+    }).success(function () {
+        alert('success');
+    });
 };
 
 var collapse = function () {
@@ -178,7 +328,15 @@ var collapse = function () {
 };
 
 var main = function () {
+    $('#addvhost').click(function () { $('#addEditVHost').show(); });
     $('#collapse').click(function () { collapse(); });
+
+    $('#vhApply').click(function () { addEditVHost(); });
+    $('#vhCancel').click(function () {
+        $('#addEditVHost').hide();
+        $('#vhUsername').val('');
+        $('#vhPassword').val('');
+    });
 
     setTimeout(function () { refresh(); }, 1000);
     setInterval(function () { refresh(); }, 30000);
